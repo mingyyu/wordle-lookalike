8b2db66c806987ec6705c591dcb4c289040627c2
mingyyu <65578403+mingyyu@users.noreply.github.com>
2023-12-03 17:20:16 +0800

    words are now checked for correct answers

--- script.js
+++ script.js
@@ -3,27 +3,35 @@ document.addEventListener("DOMContentLoaded", function () {
     var currentRow = 1;
     var currentBox = 1;
   
+    let answer = "";
+  
     // Fetch the word list and handle the submission logic
     fetch(wordListURL)
       .then((response) => response.text())
       .then((data) => {
         const wordArray = data.split("\n").filter((word) => word.length === 5);
   
+        answer =
+          wordArray[Math.floor(Math.random() * wordArray.length)].toLowerCase();
+  
+        // Initialize the game (e.g., set up UI, display instructions)
+        initializeGame();
+  
         document
           .getElementById("submitBtn")
           .addEventListener("click", function () {
-            const word = getCurrentWord();
+            const word = getCurrentWord().toLowerCase();
             if (word.length === 5) {
-              if (wordArray.includes(word.toLowerCase())) {
+              if (wordArray.includes(word)) {
                 console.log(`${word} is a valid English word!`);
+                processGuess(word);
                 moveNextRow();
               } else {
-                console.log(`${word} is not a valid English word.`);
-                displayErrorMessage();
+                displayErrorMessage(`${word} is not a valid English word.`);
                 resetRow();
               }
             } else {
-              console.log("Word must be 5 letters long.");
+              displayErrorMessage("Word must be 5 letters long.");
               // Add your logic for an invalid word length
             }
           });
@@ -31,6 +39,77 @@ document.addEventListener("DOMContentLoaded", function () {
       .catch((error) => {
         console.error("Error reading the word list:", error);
       });
+  
+    function initializeGame() {
+      // Add your game initialization logic here
+      // For example, set up the UI, display instructions, etc.
+    }
+  
+    function processGuess(guess) {
+      // Check the guess against the answer
+      const feedback = provideFeedback(guess);
+  
+      // Update the UI based on the feedback
+      updateUI(feedback);
+  
+      // Check if the guess is correct
+      if (guess === answer) {
+        showCongratsModal(currentRow);
+      }
+    }
+  
+    function provideFeedback(guess) {
+      const feedback = [];
+      const remainingOccurrences = {};
+  
+      // Initialize remainingOccurrences with the occurrences of each letter in the answer
+      for (const letter of answer) {
+        remainingOccurrences[letter] = (remainingOccurrences[letter] || 0) + 1;
+      }
+  
+      // Compare each letter in the guess with the corresponding letter in the answer
+      for (let i = 0; i < answer.length; i++) {
+        const guessedLetter = guess[i];
+  
+        if (
+          guessedLetter === answer[i] &&
+          remainingOccurrences[guessedLetter] > 0
+        ) {
+          feedback.push("correct-position");
+          remainingOccurrences[guessedLetter]--;
+        } else if (remainingOccurrences[guessedLetter] > 0) {
+          feedback.push("wrong-position");
+          remainingOccurrences[guessedLetter]--;
+        } else {
+          feedback.push("incorrect");
+          if (!answer.includes(guessedLetter)) {
+            const keyboard_key = document.getElementById(
+              guessedLetter.toUpperCase(),
+            );
+            keyboard_key.classList.add("not-included");
+          }
+        }
+      }
+  
+      return feedback;
+    }
+  
+    function updateUI(feedback) {
+      // Update the UI based on the feedback
+      for (let i = 1; i <= feedback.length; i++) {
+        const box = document.getElementById(`box${currentRow}${i}`);
+        box.classList.remove("correct-position", "wrong-position", "incorrect");
+        box.style.color = "#ffffff";
+  
+        if (feedback[i - 1] === "correct-position") {
+          box.classList.add("correct-position");
+        } else if (feedback[i - 1] === "wrong-position") {
+          box.classList.add("wrong-position");
+        } else {
+          box.classList.add("incorrect");
+        }
+      }
+    }
   
     // Function to get the current word from the boxes
     function getCurrentWord() {
@@ -59,9 +138,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   
     // Function to display an error message for 5 seconds
-    function displayErrorMessage() {
+    function displayErrorMessage(message) {
       const errorMessage = document.createElement("div");
-      errorMessage.textContent = "No such word exist";
+      errorMessage.textContent = message;
       errorMessage.className = "alert alert-secondary";
       errorMessage.style.position = "fixed";
       errorMessage.style.top = "20%";
@@ -72,6 +151,40 @@ document.addEventListener("DOMContentLoaded", function () {
       setTimeout(() => {
         document.body.removeChild(errorMessage);
       }, 3000);
+    }
+  
+    function showCongratsModal(guesses) {
+      var modalContent = document.getElementById("modalContent");
+  
+      // Replace {guesses} with the actual number of rows
+      var statsText = "WORDLE " + guesses + "/6\n";
+  
+      // Replace 🟨🟩⬜ with the actual situations in the rows and boxes
+      var row1 = "⬜⬜🟩⬜⬜";
+      var row2 = "⬜🟨🟩⬜⬜";
+      var row3 = "⬜⬜🟩⬜🟨";
+      var row4 = "⬜⬜🟩⬜⬜";
+      var row5 = "🟩🟩🟩🟩🟩";
+  
+      statsText += row1 + "\n" + row2 + "\n" + row3 + "\n" + row4 + "\n" + row5;
+  
+      modalContent.textContent = statsText;
+  
+      // Show the modal
+      $("#congratsModal").modal("show");
+    }
+  
+    function copyStats() {
+      var statsText = document.getElementById("modalContent").textContent;
+  
+      var tempInput = document.createElement("textarea");
+      tempInput.value = statsText;
+      document.body.appendChild(tempInput);
+      tempInput.select();
+      document.execCommand("copy");
+      document.body.removeChild(tempInput);
+  
+      alert("Stats copied to clipboard!");
     }
   
     // Add a click event listener to each keyboard button
