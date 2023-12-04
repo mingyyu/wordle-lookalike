245a92a877ec4c81dd7490485ae69266bfb81cb9
mingyyu <65578403+mingyyu@users.noreply.github.com>
2023-12-04 15:51:43 +0800

    added two gamemodes

--- script.js
+++ script.js
@@ -2,50 +2,85 @@ document.addEventListener("DOMContentLoaded", function () {
   const wordListURL = "words.txt"; // Adjust the URL to your word list file
   var currentRow = 1;
   var currentBox = 1;
+  var game_mode = getGameData().gameMode;
+  document.getElementById("toggleGameMode").innerHTML = ["Daily", "Unlimited"][
+    game_mode
+  ];
+  var currentDay = getCurrentDay();
+  var initialising = 1;
 
-  let answer = "";
+  function getGameData() {
+    const storedData = localStorage.getItem("gameData");
+    return storedData
+      ? JSON.parse(storedData)
+      : {
+          winsDaily: 0,
+          lossesDaily: 0,
+          gameMode: 0,
+          currentDay: -1,
+          storedGuesses: [],
+        };
+  }
 
-  // Fetch the word list and handle the submission logic
-  fetch(wordListURL)
-    .then((response) => response.text())
-    .then((data) => {
-      const wordArray = data.split("\n").filter((word) => word.length === 5);
+  // Function to update and save game statistics to localStorage
+  function updateGameData(stat, outcome) {
+    if (initialising === 1) {
+      return;
+    }
+    let gameData = getGameData();
+    console.log("accessed gameData: " + JSON.stringify(gameData));
+    if (stat == 2) {
+      gameData.gameMode = outcome;
+      localStorage.setItem("gameData", JSON.stringify(gameData));
+      return;
+    }
+    if (game_mode == 1) {
+      return;
+    }
+    if (gameData.currentDay != currentDay) {
+      gameData.currentDay = currentDay;
+      gameData.storedGuesses = [];
+    }
+    if (stat == 3) {
+      gameData.storedGuesses.push(outcome);
+    } else if (stat == 4) {
+      gameData.storedGuesses.pop();
+    } else if (stat == 0) {
+      gameData.winsDaily++;
+    } else if (stat == 1) {
+      gameData.lossesDaily++;
+    }
 
-      answer =
-        wordArray[Math.floor(Math.random() * wordArray.length)].toLowerCase();
+    // Save updated statistics to localStorage
+    localStorage.setItem("gameData", JSON.stringify(gameData));
+    console.log("new gameData: " + JSON.stringify(gameData));
+  }
 
-      // Initialize the game (e.g., set up UI, display instructions)
-      initializeGame();
-
-      document
-        .getElementById("submitBtn")
-        .addEventListener("click", function () {
-          const word = getCurrentWord().toLowerCase();
-          if (word.length === 5) {
-            if (wordArray.includes(word)) {
-              console.log(`${word} is a valid English word!`);
-              result_of_word = processGuess(word);
-              moveNextRow();
-              if (currentRow >= 7 && result_of_word === 0) {
-                showResultsModal(6, 1);
-              }
-            } else {
-              displayErrorMessage(`${word} is not a valid English word.`);
-              resetRow();
-            }
-          } else {
-            displayErrorMessage("Word must be 5 letters long.");
-            // Add your logic for an invalid word length
-          }
-        });
-    })
-    .catch((error) => {
-      console.error("Error reading the word list:", error);
-    });
+  let answer = "111111";
 
   function initializeGame() {
-    // Add your game initialization logic here
-    // For example, set up the UI, display instructions, etc.
+    stored_data = getGameData();
+    gameModeDisplay = document.getElementById("gameModeDisplay");
+    gameModeDisplay.innerHTML = ["DAILY", "UNLIMITED"][stored_data.gameMode];
+
+    if (
+      stored_data.storedGuesses != [] &&
+      stored_data.currentDay == currentDay &&
+      game_mode === 0
+    ) {
+      for (let i = 0; i < stored_data.storedGuesses.length; i++) {
+        var button = document
+          .querySelector(
+            `.keyboard-button[data-letter="${stored_data.storedGuesses[i]}"]`,
+          )
+          .click();
+        if (currentBox >= 6) {
+          document.getElementById("submitBtn").click();
+        } else {
+          console.log("not submitting");
+        }
+      }
+    }
   }
 
   function processGuess(guess) {
@@ -58,6 +93,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Check if the guess is correct
     if (guess === answer) {
       showResultsModal(currentRow, 0);
+      updateGameData(0, 0);
       return 1;
     } else {
       return 0;
@@ -146,6 +182,7 @@ document.addEventListener("DOMContentLoaded", function () {
       box_to_empty = document.getElementById(`box${currentRow}${i}`);
       box_to_empty.innerHTML = "";
       box_to_empty.style.borderColor = "#c4c4c4";
+      updateGameData(4, 0);
     }
     currentBox = 1;
   }
@@ -171,7 +208,12 @@ document.addEventListener("DOMContentLoaded", function () {
     var modalTitle = document.getElementById("resultsModalLabel");
     var modalContent = document.getElementById("modalContent");
     // Replace {guesses} with the actual number of rows
-    var statsText = "WORDLE " + guesses + "/6<br><br>";
+    var statsText =
+      "WORDLE " +
+      ["Daily " + currentDay, "Unlimited"][game_mode] +
+      " " +
+      guesses +
+      "/6<br><br>";
     for (let i = 1; i <= guesses; i++) {
       for (let j = 1; j <= 5; j++) {
         var box = document.getElementById("box" + i + j);
@@ -183,15 +225,21 @@ document.addEventListener("DOMContentLoaded", function () {
           statsText += "⬜";
         }
       }
-      statsText += "<br>";
+      statsText += "\n";
     }
 
+    const copyStatsButton = document.getElementById("copyStats");
+
+    copyStatsButton.dataset.stats =
+      statsText + "\nPlay at mingyyu.github.io/wordle-lookalike";
+
+    meanings = getWordMeaning(answer);
     modalTitle.innerHTML =
-      ["Congratulations! ", "Try again! "][result] +
-      "The word was " +
+      ["Congratulations!", "Try again!"][result] +
+      " <br>The word was " +
       answer +
       ".";
-    modalContent.innerHTML = statsText;
+    modalContent.innerHTML = "Share this wordle with others!";
 
     // Show the modal
     //$("#resultsModal").modal("show");
@@ -199,6 +247,84 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("resultsModal"),
     );
     results_modal.toggle();
+  }
+
+  function getCurrentDay() {
+    var startDateUTC = new Date("2023-12-01");
+    var endDateUTC = new Date();
+
+    startDateUTC.setUTCHours(0, 0, 0, 0);
+    endDateUTC.setUTCHours(0, 0, 0, 0);
+
+    var timeDifferenceUTC = endDateUTC.getTime() - startDateUTC.getTime();
+    var daysDifferenceUTC = timeDifferenceUTC / (1000 * 60 * 60 * 24);
+
+    var roundedDaysUTC = Math.round(daysDifferenceUTC);
+    console.log("Rounded Days UTC:", roundedDaysUTC);
+    return roundedDaysUTC;
+  }
+
+  async function getWordMeaning(word) {
+    //try {
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
+    );
+
+    if (!response.ok) {
+      throw new Error("Word not found");
+    }
+
+    const data = await response.json();
+
+    // Assuming the API response structure, you might need to adapt this based on the actual API response
+    const wordDetails = data[0];
+    const meanings = wordDetails.meanings;
+
+    const modalContent = document.getElementById("modalContent");
+    const card = createCard(word, meanings);
+    modalContent.appendChild(card);
+
+    return meanings;
+    //} catch (error) {
+    //  console.error("Error fetching word meaning:", error.message);
+    //  return null; // Handle the error based on your application's logic
+  }
+
+  function createCard(word, meanings) {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    const title = document.createElement("h5");
+    title.className = "card-title";
+    title.innerHTML = `${word}`;
+    cardBody.appendChild(title);
+
+    meanings.forEach((meaning, index) => {
+      const word_form = document.createElement("h5");
+      word_form.className = "card-subtitle";
+      word_form.innerHTML = `<br><small><i>${meaning.partOfSpeech}</i></small>`;
+      cardBody.appendChild(word_form);
+
+      const definition_list = document.createElement("ul");
+      definition_list.className = "list-unstyled";
+
+      meaning.definitions.forEach((meaning_sub, index) => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<strong>${index + 1}.</strong> ${
+          meaning_sub.definition
+        }`;
+        definition_list.appendChild(listItem);
+      });
+
+      cardBody.appendChild(definition_list);
+    });
+
+    card.appendChild(cardBody);
+
+    return card;
   }
 
   // Function to simulate button click when corresponding key is pressed
@@ -228,12 +354,12 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   document.getElementById("copyStats").addEventListener("click", function () {
-    var text_to_copy = document
-      .getElementById("modalContent")
-      .innerText.replace("<br>", "\n"); //textContent
+    const copyStatsButton = document.getElementById("copyStats");
+    var text_to_copy = copyStatsButton.dataset.stats.replace(/<br>/gi, "\n");
+
     console.log(text_to_copy);
     if (!navigator.clipboard) {
-      displayErrorMessage("Failed to copy stats");
+      displayErrorMessage("Failed to copy.");
     } else {
       navigator.clipboard
         .writeText(text_to_copy)
@@ -241,7 +367,7 @@ document.addEventListener("DOMContentLoaded", function () {
           displayErrorMessage("Copied to clipboard!"); // success
         })
         .catch(function () {
-          displayErrorMessage("Failed to copy stats"); // error
+          displayErrorMessage("Failed to copy."); // error
         });
     }
   });
@@ -257,10 +383,12 @@ document.addEventListener("DOMContentLoaded", function () {
       var box = document.getElementById("box" + currentRow + currentBox);
 
       // Check if the current box is empty
+      console.log(box + " where row=" + currentRow + " and box=" + currentBox);
       if (box.innerHTML === "") {
         // Place the letter in the current box
         box.innerHTML = letter;
         box.style.borderColor = "#a1a1a1";
+        updateGameData(3, letter);
         // Move to the next box
         currentBox++;
       } else {
@@ -303,6 +431,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // Remove the latest letter
         latestLetterBox.innerHTML = "";
         latestLetterBox.style.borderColor = "#c4c4c4";
+        updateGameData(4, 0);
 
         // Optionally, you may want to update your logic for tracking the current box
         currentBox--;
@@ -318,5 +447,64 @@ document.addEventListener("DOMContentLoaded", function () {
           }
         }
       }
+    });
+
+  // Fetch the word list and handle the submission logic
+  fetch(wordListURL)
+    .then((response) => response.text())
+    .then((data) => {
+      const wordArray = data.split("\n").filter((word) => word.length === 5);
+      if (game_mode == 0) {
+        answer_index = getCurrentDay();
+      } else {
+        answer_index = Math.floor(Math.random() * wordArray.length);
+      }
+
+      answer = wordArray[answer_index].toLowerCase();
+
+      document
+        .getElementById("submitBtn")
+        .addEventListener("click", function () {
+          const word = getCurrentWord().toLowerCase();
+          if (word.length === 5) {
+            if (wordArray.includes(word)) {
+              console.log(`${word} is a valid English word!`);
+              result_of_word = processGuess(word);
+              moveNextRow();
+              if (currentRow >= 7 && result_of_word === 0) {
+                showResultsModal(6, 1);
+                updateGameData(1, 0);
+              }
+            } else {
+              displayErrorMessage(`${word} is not a valid English word.`);
+              resetRow();
+            }
+          } else {
+            displayErrorMessage("Word must be 5 letters long.");
+            // Add your logic for an invalid word length
+          }
+        });
+
+      // Initialize the game (e.g., set up UI, display instructions)
+      initializeGame();
+      initialising = 0;
+    })
+    .catch((error) => {
+      console.error("Error reading the word list:", error);
+    });
+
+  document
+    .getElementById("switchGameMode")
+    .addEventListener("change", function () {
+      var toggleContent = document.getElementById("toggleGameMode");
+      if (game_mode == 0) {
+        game_mode = 1;
+        gameModeText = " Unlimited";
+      } else {
+        game_mode = 0;
+        gameModeText = " Daily";
+      }
+      toggleContent.innerHTML = gameModeText;
+      updateGameData(2, game_mode);
     });
 });
